3d2ed7e624169e48f184a0d19f2ba5dea5d6c118
Petr Popov <p9006557043@yandex.ru>
2025-01-24 21:15:06 +0100

    Added handlers

--- main.py
+++ main.py
@@ -1,30 +1,51 @@
 from telebot import TeleBot, types # импортировав types - импортируем типы данных из API
+from config import TOKEN
 
-# вынести в отдельный файл
-TOKEN = '7464601268:AAGo5B-_NLEM57sockMAUkhRk_Av76FJtIM'
 
-bot_commands = ['/start','/help']
+help_message = """ Вот список доступных команд:
+/start - начало работы с ботом
+/help - показать список команд 
+"""
+# команды надо прописывать в бот через BotFather через команду /setcommands
 
 bot = TeleBot(TOKEN)
 
+@bot.message_handler(commands=['start'])
+def handle_command_start(message: types.Message):
+    user_text = message.text
+    bot_answer = 'Привет!'
+    print(f'Пользователь: {user_text}')
+    print(f'Ответ: {bot_answer}')
+    bot.send_message(
+        chat_id=message.chat.id,
+        text=bot_answer
+    )
+
+@bot.message_handler(commands=['help'])
+def handle_command_help(message: types.Message):
+    user_text = message.text
+    bot_answer = help_message
+    print(f'Пользователь: {user_text}')
+    print(f'Ответ: {bot_answer}')
+    bot.send_message(
+        chat_id=message.chat.id,
+        text=help_message
+    )
+
+
 # Отправка сообщения в ответ на сообщение пользователя
 @bot.message_handler()
-def reply_send_message(message: types.Message): # обязательно передавать объект message, даже если он не используется и в явном виде указываем тип объекта
+def echo_message(message: types.Message): # обязательно передавать объект message, даже если он не используется и в явном виде указываем тип объекта
     user_text = message.text #  записываем сообщение пользователя в переменную
     print(f'Пользователь: {user_text}') # выводим в консоль сообщение пользователя
-    if user_text == bot_commands[0]: # условие при котором бот отправит сообщение в ответ пользователю
-        bot_message = 'Привет я проект бота!'
-        print(f'Ответ: {bot_message}')
-    elif user_text == bot_commands[1]:
-        bot_message = f'Вот список команд: {bot_commands}'
-        print(f'Ответ: {bot_message}')
-    else:
-        bot_message = 'Я еще не знаю этой команды :('
-        print(f'Ответ: {bot_message}')
+    bot_message = 'Я еще не знаю этой команды :('
+    print(f'Ответ: {bot_message}')
     bot.send_message(
         chat_id=message.chat.id,  # берем chat_id из сообщения присланного, пользователем
         text=bot_message
     )
+
+
 
 # if 'текст' in user_text.lower(): # если сообщение пользователя хоть где-то содержит "текст"
 #     bot.send_message(
